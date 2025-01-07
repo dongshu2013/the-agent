b552ce31aa6e493abb31fe6e84fd7971459507a4
shudong <dongs2011@gmail.com>
2025-01-07 15:38:54 -0800

    fix server start issue

--- ai_companion/main.py
+++ ai_companion/main.py
@@ -2,10 +2,14 @@ import uvicorn
 
 
 def start_server():
-    uvicorn.run("ai_companion.main:app", host="0.0.0.0", port=8000, lifespan="on")
+    uvicorn.run("ai_companion.api.app:app", host="0.0.0.0", port=8000, lifespan="on")
 
 
 def start_dev():
     uvicorn.run(
-        "ai_companion.main:app", host="0.0.0.0", port=8000, lifespan="on", reload=True
+        "ai_companion.api.app:app",
+        host="0.0.0.0",
+        port=8000,
+        lifespan="on",
+        reload=True,
     )
