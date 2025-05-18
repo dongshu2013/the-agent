56d7ae5560acf28483014161c9b9bee849d7c81c
xiangnuans <cocoguo7636@gmail.com>
2025-05-18 23:11:00 +0800

    fix open sidepanel error

--- extension/src/utils/env.ts
+++ extension/src/utils/env.ts
@@ -1,6 +1,5 @@
 import { Env } from "../types/index";
 
-
 if (!process.env.PLASMO_PUBLIC_BACKEND_URL) {
   throw new Error("PLASMO_PUBLIC_BACKEND_URL is not set");
 }
@@ -9,11 +8,8 @@ if (!process.env.PLASMO_PUBLIC_WEB_URL) {
   throw new Error("PLASMO_PUBLIC_WEB_URL is not set");
 }
 
-const DEFAULT_MODEL = "deepseek-chat";
-
 export const env: Env = {
-  DEFAULT_MODEL:
-    process.env.PLASMO_PUBLIC_DEFAULT_MODEL || DEFAULT_MODEL,
+  DEFAULT_MODEL: process.env.PLASMO_PUBLIC_DEFAULT_MODEL || "deepseek-chat",
   BACKEND_URL: process.env.PLASMO_PUBLIC_BACKEND_URL,
   WEB_URL: process.env.PLASMO_PUBLIC_WEB_URL,
 };
